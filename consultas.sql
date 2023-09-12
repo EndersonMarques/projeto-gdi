0174e873b244bc1bbf0c2374bc424a8908919678
luis-trevisan <101842136+luis-trevisan@users.noreply.github.com>
2023-09-12 20:47:59 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -8,7 +8,7 @@
 
 
 /* Junção interna:
-	Projetar o nome de todos os shows e o nome de seu respectivo festival */
+     Projetar o nome de todos os shows e o nome de seu respectivo festival */
 
         SELECT S.NOME, F.NOME
         FROM SHOW S INNER JOIN
@@ -21,8 +21,8 @@
 		SELECT F.NOME
 		FROM FUNCIONARIO F LEFT OUTER JOIN
 			TRABALHA T ON F.MATRICULA = T.MATRICULA_FUNC
-		WHERE T.DATA_ = CURRENT_DATE AND 
-            T.ID_FESTIVAL IS NULL
+		WHERE T.DATA_ = CURRENT_DATE AND
+			T.ID_FESTIVAL IS NULL
 
 /* Semi junção:
     Projetar os telefone de todos os clientes que compraram ingresso em algum festival */
@@ -30,10 +30,10 @@
         SELECT CLI.TELEFONE_1, CLI.TELEFONE_2
 		FROM CLIENTE CLI
 		WHERE EXISTS (
-            SELECT C.CPF
+    		SELECT C.CPF
 			FROM COMPRA C
 			WHERE CLI.CPF = C.CPF
-            )
+        )
 
 /* Anti-junção:
     Projetar o CPF dos clientes que possuem ingresso e não participaram da gincana
@@ -41,23 +41,24 @@
         SELECT C.CPF
         FROM COMPRA C
         WHERE NOT EXISTS (
-            SELECT P.NUM_INGRESSO
-            FROM PARTICIPA P
+        	SELECT P.NUM_INGRESSO
+        	FROM PARTICIPA P
             WHERE P.NUM_INGRESSO = C.NUM
             )
 
 /* Subconsulta do tipo escalar:
-    Projetar o nome do chefe do funcionário de matricula X*/
+    Projetar o nome do chefe do funcionário de matricula X */
+		
         SELECT F.NOME
         FROM FUNCIONARIO F
         WHERE F.MAT_CHEFE = (
             SELECT F1.MAT_CHEFE
             FROM FUNCIONARIO F1
             WHERE F1.MATRICULA = 'X'
-        )
+			)
 
 /* Subconsulta do tipo linha:
-    Projetar os shows som o mesmo horário e local do show de código X*/
+    Projetar os shows som o mesmo horário e local do show de código X */
 
         SELECT S.NOME
         FROM SHOW S
@@ -65,7 +66,7 @@
             SELECT S1.DATA_HORA, S1.LOCAL_
             FROM SHOW S1
             WHERE S1.COD = 'X'
-        )
+        	)
 
 /* Subconsulta do tipo tabela:
     Projetar o nome de todos os funcionários que trabalham trabalham atualmente em algum festival */
@@ -77,12 +78,12 @@
             FROM TRABALHA T
             WHERE T.ID_FESTIVAL IS NOT NULL AND
             T.DATA_ = CURRENT_DATE
-        )
+        	)
 
 /* Operação de Conjunto:
     Projetar o nome de todos os cantores que são músicos */
         
-            SELECT C.NOME
+        	SELECT C.NOME
             FROM CANTOR C
         UNION
             SELECT M.NOME
